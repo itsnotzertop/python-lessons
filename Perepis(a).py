c854d7f2cf2080b4f716435f3cc267c9156a63a2
zertop <soxpav00@gmail.com>
2023-01-20 17:08:31 +0600

    debug

--- Perepis(a).py
+++ Perepis(a).py
@@ -1,11 +1,9 @@
 f = open('Perepis.txt','r')
 summ = 0
-minn =int(input())
-maxx =int(input())
 for i in f:
     data = i[len(i)-5::]
-    if minn<int(data) < maxx:
+    if int(data) < 1978:
         base = i.split()
         summ +=1
-        print(i)
+        print(base[2],base[3])
 print(summ)
